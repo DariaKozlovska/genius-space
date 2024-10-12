88c7bf286bf382757622cacb0a4e46a49653e182
DariaKozlovska <odriy.lovska@gmail.com>
2024-10-12 13:09:53 +0200

    fix bugs

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,22 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
+const menuLink = document.querySelectorAll(".mobile-menu__item");
 
-const toggleModal = () => mobileMenu.classList.toggle('is-open');
+const toggleModalMobile = () => mobileMenu.classList.toggle("is-open");
 
-menuBtnOpen.addEventListener('click', toggleModal);
-menuBtnClose.addEventListener('click', toggleModal);
+menuBtnOpen.addEventListener("click", toggleModalMobile);
+menuBtnClose.addEventListener("click", toggleModalMobile);
+menuLink.forEach((element) => {
+  element.addEventListener("click", toggleModalMobile);
+});
+
+const modal = document.querySelector(".backdrop");
+const modalBtnOpen = document.querySelectorAll(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+modalBtnOpen.forEach((e) => {
+  e.addEventListener("click", toggleModal);
+});
+modalBtnClose.addEventListener("click", toggleModal);
